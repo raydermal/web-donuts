cad189c5a9d8e1fa24c6e09fb040a2e7ed273073
raydermal <raydermal@gmail.com>
2023-04-21 18:03:28 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    $('#menus').click(function(){
+        $('.left-siderbar').slideToggle();
+    
+    })
+
+    $('.menu-2 #menu-4').click(function(){
+        $('.left-siderbar').slideUp();
+
+    })
+})
+
